07312be9f2e714b4a473996753e6521af78ae895
kevindoherty <kvdoherty@gmail.com>
2014-09-06 14:26:37 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Homebuyer::Application.config.secret_key_base = '58f9c9581104ac47f6fe8069cdd16a80c22955b3182cf2a09d2d03b2bfb2c88c5cf8e0f164dd54616a74460640c986a0caad3fa417b95972cb0b16d705e41ff7'
